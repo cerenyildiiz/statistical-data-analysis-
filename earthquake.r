f50c0493aab7d4d1d21b8335a98d8d332ce45959
Ceren Yildiz <cerennyildiz634@gmail.com>
2026-07-19 17:26:43 +0300

    Update earthquake.r

--- earthquake.r
+++ earthquake.r
@@ -1,4 +1,11 @@
 
-    # Import the earthquake dataset from a .txt file 
+# Import the earthquake dataset from a .txt file 
 # Read a txt file, named "mugla.txt"
+
+# This code will read the dataset as if it contains only one variable
+
 mugla <- read.delim("mugla.txt")
+
+
+# To import the data without errors, you need to apply the following code 
+mugla <- read.delim("mugla.txt", header = TRUE, sep = "", row.names = 1)
